c2398e5c8e8f1b79f8e6655139fb3907768ac950
Hudoberdi1122 <nabijonovhudoberdi9@gmail.com>
2023-08-17 15:29:00 +0500

    Consol

--- backend/views/coment/index.php
+++ backend/views/coment/index.php
@@ -17,9 +17,6 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Coment', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
 
 
     <?= GridView::widget([
