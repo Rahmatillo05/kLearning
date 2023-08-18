f7b1b7a63fcd393994f899602d4deda5d183b8bf
Hudoberdi1122 <nabijonovhudoberdi9@gmail.com>
2023-08-18 20:37:18 +0500

    Salom

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -11,31 +11,16 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="container">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
     <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div class="my-1 mx-0" style="color:#999;">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                    <br>
-                    Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
-                </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+        <div class="col-6 my-3 offset-3">
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                    <?= $form->field($model, 'username')->textInput(['style' =>'border-radius:20px;','placeholder'=>'username...']) ?>
+                    <?= $form->field($model, 'password')->passwordInput(['style' =>'border-radius:20px;','placeholder'=>'password...']) ?>
+                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                    <div class="form-group text-right">
+                        <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    </div>
+                <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
