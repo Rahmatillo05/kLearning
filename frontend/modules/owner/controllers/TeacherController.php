4ea2e2fb3bbe5626322680ee63d2cb8b9a861c45
Hudoberdi1122 <nabijonovhudoberdi9@gmail.com>
2023-08-19 16:01:22 +0500

    Ozgarish kotta

---
+++ frontend/modules/owner/controllers/TeacherController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace frontend\modules\owner\controllers;
+
+use backend\controllers\BaseController;
+use common\models\user\TeacherSocialAccounts;
+use common\models\user\User;
+use common\models\user\UserInfo;
+use common\models\user\UserRole;
+use common\widgets\Detect;
+use common\widgets\Tools;
+use common\widgets\UploadFile;
+use Yii;
+use yii\base\Exception;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
+use yii\web\UploadedFile;
+
+class TeacherController extends BaseController
+{
+    public function actionIndex(): string
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => User::find()->where(['role' => Detect::TEACHER]),
+
+            'pagination' => [
+                'pageSize' => 50
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'id' => SORT_DESC,
+                ]
+            ]
+
+        ]);
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+    /**
+     * @throws Exception
+     */
+    public function actionCreate(): Response|string
+    {
+        $model = new User();
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post())) {
+                $model->username = Tools::generateUsername($model->full_name);
+                $model->setPassword($model->username);
+                $model->generateAuthKey();
+                if ($model->save()) {
+                    return $model->role == Detect::PUPIL ? $this->redirect(['add-parent', 'pupil_id' => $model->id]) : $this->redirect(['view', 'id' => $model->id]);
+                }
+            }
+        } else {
+            $model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+    public function actionUpdate(int $id): Response|string
+    {
+        $model = $this->findModel($id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+    public function actionDelete(int $id): Response
+    {
+        $user = $this->findModel($id);
+        $user->status = User::STATUS_DELETED;
+        $user->save();
+        return $this->redirect(['index']);
+    }
+    public function actionView(int $id): string
+    {
+        $model = $this->findModel($id);
+        return $this->render('view', compact('model'));
+    }
+    protected function findModel(int $id): User
+    {
+        if (($model = User::findOne(['id' => $id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
---
+++ frontend/modules/owner/views/teacher/index.php
@@ -0,0 +1,55 @@
+<?php
+
+use common\models\user\User;
+use common\widgets\Tools;
+use yii\bootstrap5\Html;
+use yii\bootstrap5\LinkPager;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\helpers\Url;
+
+?>
+<p>
+    <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+</p>
+<div class="table-responsive">
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'tableOptions' => [
+            'class' => 'table table-bordered'
+        ],
+        'columns' => [
+            [
+                'class' => 'yii\grid\SerialColumn',
+                'header' => 'T/r'
+            ],
+            'full_name',
+            'username',
+            'tel_number',
+            [
+                'attribute' => 'status',
+                'value' => function (User $model) {
+                    return Tools::setStatusBadge($model->status);
+                },
+                'format' => 'html'
+            ],
+            [
+                'attribute' => 'role',
+                'value' => function (User $model) {
+                    return Tools::ruleName($model->role);
+                }
+            ],
+            'created_at:date',
+            [
+                'class' => ActionColumn::class,
+                'urlCreator' => function ($action, User $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                },
+                'buttons' => Tools::gridViewButtons()
+            ],
+        ],
+        'pager' => [
+            'class' => LinkPager::class
+        ]
+    ]); ?>
+</div>
