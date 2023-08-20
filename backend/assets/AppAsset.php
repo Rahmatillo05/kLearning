07bc4207c78509a512d58371ed6183e463458067
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-08-20 23:29:24 +0500

    add some changes on dtm system

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,7 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css',
-        'https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css',
+//        'https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css',
         'css/styles.min.css',
     ];
     public $js = [
@@ -26,10 +26,10 @@ class AppAsset extends AssetBundle
 //        'https://code.jquery.com/jquery-3.5.1.js',
         'https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js',
         'https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js',
-        'https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js',
-        'https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js',
+//        'https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js',
+//        'https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js',
+//        'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js',
+//        'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js',
         'js/datatable.js'
     ];
     public $depends = [
