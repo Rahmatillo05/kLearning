78c7bd6764bbbc8918cefc8ca4e5fa4b455d66b6
Rahmatullo Husanboyev <hrrahmatillo2oo5@gmail.com>
2023-09-02 08:15:38 +0500

    fixed bugs

--- backend/views/payment/update.php
+++ backend/views/payment/update.php
@@ -10,12 +10,14 @@ $this->params['breadcrumbs'][] = ['label' => 'Payments', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="payment-update">
+<div class="card">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 class="card-title card-header"><?= Html::encode($this->title) ?></h1>
+    <div class="card-body">
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+        <?= $this->render('_form', [
+            'model' => $model,
+        ]) ?>
 
+    </div>
 </div>
