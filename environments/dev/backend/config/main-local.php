dc985ab8766e64a8903941883ab846fb129d716c
Rahmatullo Husanboyev <hrrahmatillo2oo5@gmail.com>
2023-09-02 21:49:11 +0500

    add some changed

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -5,6 +5,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
+            'baseUrl' => '/backend'
         ],
     ],
 ];
